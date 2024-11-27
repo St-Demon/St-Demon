c5a1551b59071b70aba64f983f1e046e0500bd48
Dongjin <plan004913@gmail.com>
2024-11-27 16:01:31 +0900

    주소수정

--- components/ChatWindow.jsx
+++ components/ChatWindow.jsx
@@ -16,7 +16,7 @@ const ChatWindow = () => {
             setMessages((prevMessages) => [...prevMessages, { text: message, type: 'outgoing' }]);
 
             // 서버에 메시지를 보내고 응답을 처리
-            fetch('https://backend.dongjinhub.store/chat', {
+            fetch('https://backend.dongjinhub.store', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
